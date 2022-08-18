0c31b2ac699701cfb3acd1d36411dc24aa2c0768
Anna Laffoon <ree11194@gmail.com>
2022-08-18 12:41:15 -0400

    keydown listener

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import data from './data.js'
 
+//returns only the "emoji" and "category" property from the objects in the original data array
 let remainingEmojis = data.map(obj => {
     return {
         emoji: obj.emoji,
@@ -10,15 +11,10 @@ let remainingEmojis = data.map(obj => {
 shuffleArray(remainingEmojis);
 
 let correctAnswers = []
-
 let highScore = 0;
-
 let currentEmoji;
 
 displayNextEmoji()
-
-console.log(correctAnswers)
-
 
 // using Durstenfeld shuffle based on the implentation found here: https://stackoverflow.com/a/12646864
 function shuffleArray(array) {
@@ -28,6 +24,7 @@ function shuffleArray(array) {
     }
 }
 
+//shows the subsequent emoji in the array 
 function displayNextEmoji() {
     currentEmoji = remainingEmojis.pop()
     document.getElementById("emojifig").innerHTML = currentEmoji.emoji;
@@ -36,9 +33,10 @@ function displayNextEmoji() {
         return obj.emoji;
     })
     document.getElementById("score").innerHTML = `Your score is ${correctAnswers.length}. The high score is ${highScore}.`
-    document.getElementById("answers").innerHTML = emojis.join(", ");
+    document.getElementById("answers").innerHTML = emojis.join(" ");
 }
 
+//returns to the original state of the game with answers reset
 function resetRemainingEmojis() {
     remainingEmojis = data.map(obj => {
         return {
@@ -50,6 +48,8 @@ function resetRemainingEmojis() {
 }
 
 
+//if player guesses correctly, currentEmoji is added to the correctAnswers array and displayed, as well as a count of the 
+//high score. Then the next emoji is shown. If player is incorrect, everything is reset and they must start over.
 function handleAnswer(isFlag) {
     let currentEmojiIsFlag = currentEmoji.category === "Flags";
 
@@ -68,5 +68,13 @@ function handleAnswer(isFlag) {
         alert("You lose! Sorry")
     }
 }
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "y") {
+        handleAnswer(true)
+    } else if (event.key === "n") {
+        handleAnswer(false)
+    }
+});
 
 window.handleAnswer = handleAnswer;
